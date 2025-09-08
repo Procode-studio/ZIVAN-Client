7bdb9f7e715dbd2561d8e84c852909355209668d
Ivan <vanozdar23@gmail.com>
2025-09-08 20:19:14 +0500

    fix

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -13,17 +13,14 @@ const peerConfig = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     {
-      urls: 'turn:staticauth.openrelay.metered.ca:80',
-      credential: 'openrelayprojectsecret'
-    },
-    {
-      urls: 'turn:staticauth.openrelay.metered.ca:443',
-      credential: 'openrelayprojectsecret'
-    },
-    {
-      urls: 'turn:staticauth.openrelay.metered.ca:443?transport=tcp',
-      credential: 'openrelayprojectsecret'
-    }
+      urls: [
+        'turn:openrelay.metered.ca:80',
+        'turn:openrelay.metered.ca:443',
+        'turn:openrelay.metered.ca:443?transport=tcp'
+      ],
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+	}
   ]
 };
 
